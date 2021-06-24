de568a67260a79702d5dbf8daba527d9c1bcf9bb
Devanshu Gupta <43664122+devanshu00@users.noreply.github.com>
2021-06-25 01:34:24 +0530

    Add files via upload

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+dbPassword = 'mongodb+srv://devamshu00:'+ encodeURIComponent('kkkkkkkkkkkk') + '@cluster0.4mjzn.mongodb.net/test?retryWrites=true';
+
+module.exports = {
+    mongoURI: dbPassword
+};
+
